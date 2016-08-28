d8492e08252258798d292368a2b050ac4eda8ab1
mampa <mampa@MUHAYMINMAC>
2016-08-28 11:08:36 +0530

    Add project files.

---
+++ InStore/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using InStore.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace InStore.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
